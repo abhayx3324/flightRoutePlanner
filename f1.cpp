822b256692b7913ad7013a709a8b1d6277bdd682
Abhay V Nikkam <132347085+abhayx3324@users.noreply.github.com>
2024-07-04 22:58:57 +0530

    Added initial incomplete code. Added CSV library

--- f1.cpp
+++ f1.cpp
@@ -1,9 +1,45 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <fstream>
+#include "csv-parser/include/csv.hpp"
 
 using namespace std;
+using namespace csv;
+
+class Airport   {
+    string name;
+    float latitude;
+    float longitude;
+
+    void add_airport()
+    {
+        printf("Enter Airport Name: ");
+        scanf("%s", name);
+        printf("Enter Latitude: ");
+        scanf("%f", &latitude);
+        printf("Enter Longitude: ");
+        scanf("%f", &longitude);
+    }
+};
+
+class Edge {
+    string destination;
+    float distance;
+};
+
+void read_airport_file()
+{
+    ifstream infile("airports.txt");
+    if (!infile)    {
+        printf("File failed to open\n");
+        return;
+    }
+    
+}
 
 int main()
 {
+    vector<Airport> graph;
     
 }
