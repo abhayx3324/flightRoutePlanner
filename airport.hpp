5c45531d63347b40ead30097553f480f2265e5eb
abhayx3324 <abhay@LAPTOP-REM4BNBE>
2024-07-20 11:59:55 +0530

    Created adjacency list from list of aiports

--- airport.hpp
+++ airport.hpp
@@ -1,9 +1,11 @@
+#define _USE_MATH_DEFINES
+
 #include <iostream>
 #include <vector>
 #include <string>
 #include <exception>
 #include "csv-parser/single_include/csv.hpp"
-#include "variadic-table/include/VariadicTable.h"
+#include "variadic_table/include/VariadicTable.h"
 
 using namespace std;
 using namespace csv;
@@ -48,3 +50,15 @@ void read_airport_file(vector<Airport>& airports, const string& filename)
     }
 }
 
+double haversine_distance(double lat1, double lon1, double lat2, double lon2) {
+    const double earthRad = 6371.0; 
+    double dLat = (lat2 - lat1) * M_PI / 180.0;
+    double dLon = (lon2 - lon1) * M_PI / 180.0;
+    lat1 = lat1 * M_PI / 180.0;
+    lat2 = lat2 * M_PI / 180.0;
+
+    double a = sin(dLat / 2) * sin(dLat / 2) +
+               sin(dLon / 2) * sin(dLon / 2) * cos(lat1) * cos(lat2);
+    double c = 2 * atan2(sqrt(a), sqrt(1 - a));
+    return earthRad * c;
+}
--- main.cpp
+++ main.cpp
@@ -1,16 +1,26 @@
-#include "airport.hpp"
+#include "graph.hpp"
 
 int main()
 {
-    vector<Airport> airports;
+    Graph graph;
     string filename = "airports.csv";
 
+    vector<Airport> airports;
     read_airport_file(airports, filename);
-    
+
+    for (const auto& airport : airports) {
+        graph.add_airport(airport);
+    }
+
+    graph.create_adjacency_list();
+
     VariadicTable<std::string, double, double> vt({"Airport", "Latitude", "Longitude"}, 10);
-    for (const auto& airport : airports)
+    for (const auto& airport : graph.airports)
         airport.display(vt);
     vt.print(cout);
+
+    cout << "\nAdjacency List:\n";
+    graph.display_adjacency_list();
 
     return 0;
 }
